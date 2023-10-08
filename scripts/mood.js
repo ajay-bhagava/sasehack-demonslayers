01878b5b90c75ef5d49aa038643c7c2b63c2f34b
ajay-bhagava <ajay.bhagava@gmail.com>
2023-10-08 18:25:57 -0400

    Transition to answering prompt implemented

--- scripts/mood.js
+++ scripts/mood.js
@@ -5,15 +5,24 @@ console.log(firstMoodButton)
 
 let regenerateResponseButton = document.getElementById("")
 
-firstMoodButton.addEventListener("click", (e) => {
+firstMoodButton.addEventListener("click", async (e) => {
     //store the mood
     let moodInput = document.querySelector("input");
     let moodValue = getMood(moodInput);
     localStorage.setItem("firstMood", moodValue);
-    console.log(moodValue);
     // bring up prompt
     let wrapperElement = document.getElementsByClassName("wrapper")[0]
-    wrapperElement.innerHTML = '<h1 id="prompt">'+getRandomPrompt()+'</h1><div class="section"'+'<div class="section"> <input type="text" id="promptAnswer" placeholder="Enter your thoughts"> <button class="submit" id="submitPrompt" onclick="">Submit</button> <button class="submit" id="regeneratePrompt" onclick="">Regenerate Prompt</button> </div>'
+    let newInnerText;
+    await fetch("../pages/answerPrompt.html").then(response => response.text()).then(result => {
+        console.log(result);
+        newInnerText = result;
+        console.log(newInnerText)    
+    });
+
+    wrapperElement.innerHTML = newInnerText;
+    document.getElementById("prompt").innerText = getRandomPrompt();
+
+    
 });
 
 let getMood = function(moodInput) {
