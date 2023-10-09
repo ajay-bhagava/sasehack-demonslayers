15068d46f0dd35f73beda2efe59b87de235ab26e
ajay-bhagava <ajay.bhagava@gmail.com>
2023-10-08 20:27:47 -0400

    Added AIresults page and interactivity

--- scripts/mood.js
+++ scripts/mood.js
@@ -1,9 +1,44 @@
 import { getRandomPrompt } from './ghettoapi.js';
 
-let firstMoodButton = document.getElementById("firstMoodButton");
-console.log(firstMoodButton)
+let update = function () {
+    const body = document.querySelector("body");
+    const emoji = document.querySelector(".slide-emoji");
+    const input = document.querySelector("input");
+    const bar = document.querySelector(".progress-bar");
+    const thumb = document.querySelector(".thumb");
+    input.oninput = () => {
+        let sliderValue = input.value;
+        thumb.style.left = sliderValue + '%';
+        bar.style.width = sliderValue + '%';
+        if (sliderValue < 20) {
+            emoji.style.marginTop = "0px";
+            body.classList.add("angry");
+            body.classList.remove("confuse");
+            body.classList.remove("like");
+        }
+        if (sliderValue >= 20) {
+            emoji.style.marginTop = "-140px";
+            body.classList.add("confuse");
+            body.classList.remove("angry");
+            body.classList.remove("like");
+        }
+        if (sliderValue >= 40) {
+            emoji.style.marginTop = "-280px";
+        }
+        if (sliderValue >= 60) {
+            emoji.style.marginTop = "-420px";
+            body.classList.add("like");
+            body.classList.remove("confuse");
+            body.classList.remove("angry");
+        }
+        if (sliderValue >= 80) {
+            emoji.style.marginTop = "-560px";
+        }
+    }
+}
 
-let regenerateResponseButton = document.getElementById("")
+let firstMoodButton = document.getElementById("firstMoodButton");
+let oldInnerText
 
 firstMoodButton.addEventListener("click", async (e) => {
     //store the mood
@@ -12,35 +47,52 @@ firstMoodButton.addEventListener("click", async (e) => {
     localStorage.setItem("firstMood", moodValue);
     // bring up prompt
     let wrapperElement = document.getElementsByClassName("wrapper")[0]
+    oldInnerText = wrapperElement.innerHTML;
+    console.log(oldInnerText)
     let newInnerText;
     await fetch("../pages/answerPrompt.html").then(response => response.text()).then(result => {
-        console.log(result);
         newInnerText = result;
-        console.log(newInnerText)    
     });
 
     wrapperElement.innerHTML = newInnerText;
     let promptHeader = document.getElementById("prompt")
     promptHeader.innerText = getRandomPrompt();
-    document.getElementById("submitPrompt").addEventListener("click", submitPrompt)
+    document.getElementById("submitPrompt").addEventListener("click", getPromptResponse)
     let responsebutton = document.getElementById("regeneratePrompt")
     responsebutton.addEventListener("click", regeneratePrompt)
-    console.log("bruh")
 });
 
-let getMood = function(moodInput) {
-    return moodInput.value / 20 >> 0
+let getMood = function (moodInput) {
+    return moodInput.value / 20 >> 0 // bitshift rounds to integer
 }
 
-let getPromptResponse = function() {
+let getPromptResponse = function (e) {
     let prompt = document.getElementById("prompt").innerText
     let response = document.getElementById("response").value
     localStorage.setItem("prompt", prompt);
     localStorage.setItem("response", response);
+    let wrapper = document.getElementById("wrapperID");
+    wrapper.innerHTML = oldInnerText;
+    document.getElementById("question").innerText = "How do you feel after reflecting on your day?"
+    update();
+    let submitButton = document.getElementById("firstMoodButton");
+    submitButton.addEventListener("click", showresults);
 }
 
-let regeneratePrompt = function(e) {
+let regeneratePrompt = function (e) {
     let promptHeader = document.getElementById("prompt")
     promptHeader.innerText = getRandomPrompt();
 }
 
+let showresults = async function (e) {
+    let moodInput = document.querySelector("input");
+    let moodValue = getMood(moodInput);
+    localStorage.setItem("secondMood", moodValue);
+    let results;
+    let increase = localStorage.getItem("secondMood") - localStorage.getItem("firstMood");
+    await fetch("../pages/aiResults.html").then(response => response.text()).then(result => results = result);
+    document.getElementById("wrapperID").innerHTML = results;
+    document.getElementById("increase").innerText = increase;
+}
+
+update();
